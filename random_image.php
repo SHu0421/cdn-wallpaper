0e56b26b3f0dfd1f1f697e8c856a981113980767
SHu <1320517578@qq.com>
2023-11-29 14:52:56 +0800

    Update random_image.php

--- random_image.php
+++ random_image.php
@@ -5,19 +5,21 @@ $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
 // 构建模拟图片路径的数据
 $images = [];
-for ($i = 1; $i <= 124; $i++) {
-    $images[$i] = 'https://github.com/SHu0421/cdn-wallpaper/blob/master/' . $i . '.jpg';
+for ($i = 0; $i <= 100; $i++) {
+    $images[$i] = 'https://github.com/SHu0421/cdn-wallpaper/blob/master/' . ++$i . '.jpg?raw=true';
 }
-
-
-// $images = [
-//     'image1.jpg',
-//     'image2.png',
-//     'image3.jpg',
-//     // Add more image paths as needed
-// ];
 
 $randomImage = $images[array_rand($images)];
 
-header('Content-Type: image/jpg'); // Adjust content type based on your image types
-readfile($randomImage);
+
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+default:
+    die(header("Location: $pic"));
+}
+?>
